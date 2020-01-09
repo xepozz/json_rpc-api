19873a0fe5830578f34cab84df4d0d9bb9d10ac8
Dmitriy Derepko <xepozz@list.ru>
2020-01-09 08:03:02 +0300

    Почистил

--- src/Models/Links.php
+++ src/Models/Links.php
@@ -6,20 +6,9 @@ namespace Rpc\Models;
 
 use Phalcon\Mvc\Model;
 
-/**
- * EmailConfirmations
- * Stores the reset password codes and their evolution
- * @method static Links findFirstByCode(string $code)
- */
 class Links extends Model
 {
     public $id;
     public $uniqueId;
     public $link;
-    public $createdAt;
-
-    public function beforeValidationOnCreate()
-    {
-        $this->createdAt = time();
-    }
 }
